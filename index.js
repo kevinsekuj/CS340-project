62faf1b10e5e4827f55c4837aaa4b5ef7bc6c4ca
kevinsekuj <kevinsekuj@gmail.com>
2021-10-28 17:04:24 -0400

    create webapp skeleton

--- index.js
+++ index.js
@@ -7,19 +7,26 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 const path = require("path");
+const methodOverride = require("method-override");
+const home = require("./routes/home");
+const ejsMate = require("ejs-mate");
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join((__dirname, "public"))));
 
-app.listen(3000, (req, res) => {
+app.use(methodOverride("_method"));
+app.use("/home", home);
+
+app.listen(port, (req, res) => {
   console.log(`Listening on port ${port}`);
 });
 
 app.get("/", (req, res) => {
-  res.send("hey");
+  res.redirect("home");
 });
 
 app.all("*", (req, res, next) => {
