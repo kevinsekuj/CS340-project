ef2baf45d79813b77f5f5fc7403cc502af2aca66
Daniel Sarran <daniel.sarran@gmail.com>
2021-11-01 23:28:33 -0700

    Add dbcon.js DB connection boilerplate

---
+++ utils/dbcon.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2/promise");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
+module.exports = async () => {
+  const con = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+  return con;
+};
