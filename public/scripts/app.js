12c1a004b273b9aaa7e694b6322b30e3cf448d7d
kevinsekuj <kevinsekuj@gmail.com>
2021-11-03 03:20:21 -0400

    add in-line edit functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
-console.log("scripts loaded");
+const edit = document.getElementById("editButton");
+
+edit.addEventListener("click", () => {
+  edit.blur();
+  const row = edit.parentElement.parentElement;
+  const artist = row.children[1];
+  const newArtist = document.createElement("td");
+  newArtist.innerHTML = `<input type="text" autofocus value="${artist.innerText}" 
+  onmouseover="this.setSelectionRange(this.value.length,this.value.length);" 
+  onfocus="this.setSelectionRange(this.value.length,this.value.length);"">`;
+  row.replaceChild(newArtist, artist);
+  newArtist.focus();
+});
