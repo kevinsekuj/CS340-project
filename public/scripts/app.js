996ed489305943f53db3802c27392d76a442a977
kevinsekuj <kevinsekuj@gmail.com>
2021-11-03 03:28:48 -0400

    add preliminary inline delete functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,21 @@
 const edit = document.getElementById("editButton");
+const del = document.getElementById("deleteButton");
 
 edit.addEventListener("click", () => {
   edit.blur();
   const row = edit.parentElement.parentElement;
   const artist = row.children[1];
   const newArtist = document.createElement("td");
+
   newArtist.innerHTML = `<input type="text" autofocus value="${artist.innerText}" 
   onmouseover="this.setSelectionRange(this.value.length,this.value.length);" 
   onfocus="this.setSelectionRange(this.value.length,this.value.length);"">`;
+
   row.replaceChild(newArtist, artist);
   newArtist.focus();
+});
+
+del.addEventListener("click", () => {
+  const row = document.getElementById("a1");
+  row.parentElement.removeChild(row);
 });
