d474b4b9856bb0c357af8a5ec761f0b3b9c50403
kevinsekuj <kevinsekuj@gmail.com>
2021-11-04 03:22:35 -0400

    a

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -1,3 +1,9 @@
 module.exports.index = (req, res) => {
-  res.render("tables/artists");
+  const fakeData = [
+    { artistId: 1, artistName: "Crystal Castles", labelId: 2 },
+    { artistId: 2, artistName: "Hikaru Utada", labelId: 1 },
+    { artistId: 3, artistName: "Kanye West", labelId: 53 },
+  ];
+
+  res.render("tables/artists", { fakeData });
 };
