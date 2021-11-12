3ae88afe605261c852c28c9405bd7fb7d4c766bd
Daniel Sarran <daniel.sarran@gmail.com>
2021-11-11 16:21:24 -0800

    Add names of songs and artists on SONGS_ARTISTS table, update query in model, table in ejs file

--- controllers/songArtistController.js
+++ controllers/songArtistController.js
@@ -2,6 +2,5 @@ const SongArtist = require("../models/songArtist");
 
 module.exports.index = async (req, res) => {
   const data = await SongArtist.readAll();
-  console.log(data);
   res.render("tables/songsArtists", { data });
 };
--- models/songArtist.js
+++ models/songArtist.js
@@ -7,7 +7,13 @@ const SongArtist = {
   readAll: async () => {
     const con = await connection();
 
-    const query = `SELECT * FROM songs_artists;`;
+    // const query = `SELECT * FROM songs_artists;`;
+    const query = `
+      SELECT s.songName, sa.songID, sa.artistID, a.artistName 
+      FROM songs_artists sa
+      JOIN songs s ON sa.songID = s.songID
+      JOIN artists a ON sa.artistID = a.artistID;`;
+
     const [rows, fields] = await con.execute(query);
 
     await con.end();
