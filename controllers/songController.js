d3210e619aa987f352efe1882ec0ff7559f680ee
Daniel Sarran <daniel.sarran@gmail.com>
2021-11-18 23:24:55 -0800

    Add create export to Songs controller

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,7 +1,19 @@
-const Song = require('../models/song');
+const Song = require("../models/song");
 
 module.exports.index = async (req, res) => {
   const data = await Song.readAll();
 
-  res.render('tables/songs', { data });
+  res.render("tables/songs", { data });
+};
+
+module.exports.create = async (req, res) => {
+  const { songName, songLength, albumSelect } = req.body;
+
+  await Song.create({
+    songName: songName,
+    songLength: songLength,
+    albumId: albumSelect,
+  });
+
+  res.redirect("/song");
 };
