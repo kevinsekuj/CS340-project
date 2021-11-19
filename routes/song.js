83dedd785c64a2a34c6cfcd7dd2899a376a5a3be
Daniel Sarran <daniel.sarran@gmail.com>
2021-11-18 23:26:33 -0800

    Add post verb to song route

--- routes/song.js
+++ routes/song.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const song = require("../controllers/songController");
 
-router.route("/").get(song.index);
+router.route("/").get(song.index).post(song.create);
 
 module.exports = router;
