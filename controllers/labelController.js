e0ed5f69219f58595c2f9195d452aac695360efc
Daniel Sarran <daniel.sarran@gmail.com>
2021-11-18 17:22:55 -0800

    Add labelController create export

--- controllers/labelController.js
+++ controllers/labelController.js
@@ -1,7 +1,18 @@
-const Label = require('../models/label');
+const Label = require("../models/label");
 
 module.exports.index = async (req, res) => {
   const data = await Label.readAll();
 
-  res.render('tables/labels', { data });
+  res.render("tables/labels", { data });
+};
+
+module.exports.create = async (req, res) => {
+  const { labelName, dateFounded } = req.body;
+
+  await Label.create({
+    labelName: labelName,
+    dateFounded: dateFounded,
+  });
+
+  res.redirect("label");
 };
