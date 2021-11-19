4d6d6e91133712e2b421b0cae4f015a07d6fe9e9
Daniel Sarran <daniel.sarran@gmail.com>
2021-11-18 23:50:38 -0800

    Add requirement to enter a search string, and add placeholder text for UI

--- models/artist.js
+++ models/artist.js
@@ -7,7 +7,7 @@ const Artist = {
   find: async (userSearch) => {
     const con = await connection();
 
-    const query = `SELECT * FROM artists WHERE artistName LIKE '%${userSearch}%';`;
+    const query = `SELECT * FROM artists WHERE artistName LIKE '${userSearch}%';`;
     const [rows] = await con.execute(query);
 
     await con.end();
